a29d3613c508cf67de77647b2130b5359546adc5
Tristanaer <2478469578@qq.com>
2019-12-24 10:08:28 +0800

    新建工程

---
+++ visual_studio_2017_sdl/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <SDL.h>
+
+
+
+
+int main(int argc, char** argv)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
+
